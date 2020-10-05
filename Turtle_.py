0492f1ab6daaa2ddbf6cfe4fb989670574f0556b
Anonymous <56997801+anonymous1996hack@users.noreply.github.com>
2020-10-05 08:36:08 +0530

    Constraint updatation

--- Turtle_.py
+++ Turtle_.py
@@ -2,21 +2,21 @@ import turtle
 
 kashyap = turtle.Turtle()
 kashyap.speed(0)
-kashyap.pencolor("red")
+kashyap.pencolor("blue")
 
 for i in range(60):
     kashyap.forward(75)
     kashyap.left(123) 
     
-kashyap.pencolor("blue")
+kashyap.pencolor("red")
 for i in range(100):
     kashyap.forward(120)
     kashyap.left(123)
 
 kashyap.pencolor("green")
 for i in range(200):
-    kashyap.forward(300)
-    kashyap.left(123)
+    kashyap.forward(350)
+    kashyap.left(124)
     
 turtle.done()
 
